5e13943be45407469c26d742d5e235f4475f01ad
Gaëtan Maisse <gmaisse@leadformance.com>
2019-05-13 20:36:08 +0200

    refactor: ♻️ remove all refs to TSLint in source code

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,3 @@
-/* eslint-disable no-console */
 // Maybe you can do something nice here!
-// tslint:disable-next-line:no-console
+// eslint-disable-next-line no-console
 console.log('Hello world');
